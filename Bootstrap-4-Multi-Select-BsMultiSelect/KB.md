3fdaf548be9b71b5ac285a2425d7a4e5aeb4e128
cjnmstl <81238817+cjnmstl@users.noreply.github.com>
2021-03-26 21:57:59 -0400

    Cleaned up Select-Ingredients file
    When through the 'Bootstrap-4-Multi-Select-BsMultiSelect' folder and removed the files that did not impact functionality.

--- Bootstrap-4-Multi-Select-BsMultiSelect/KB.md
+++
@@ -1,2 +0,0 @@
-# RTL
-https://www.rtlstyling.com/posts/rtl-styling/ (RU: https://habr.com/ru/post/484886/ )
--- Bootstrap-4-Multi-Select-BsMultiSelect/js/src/MultiSelectBuilder.js
+++
@@ -1,9 +0,0 @@
-import {composeSync} from './ToolsJs';
-import {EventBinder, EventLoopFlag, containsAndSelf} from './ToolsDom'
-
-export function MultiSelectBuilder (
-    aspects
-    ) 
-{
-    
-}
